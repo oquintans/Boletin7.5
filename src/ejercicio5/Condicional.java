7c27b0460571c61da9cdc333011a9a1096096d4b
oquintansocampo <oquintansocampo@Eniac28.danielcastelao.org>
2014-11-06 11:33:37 +0100

    corregido

--- src/ejercicio5/Condicional.java
+++ src/ejercicio5/Condicional.java
@@ -1,6 +1,3 @@
-
-
-
 package ejercicio5;
 
 import javax.swing.JOptionPane;
@@ -10,28 +7,36 @@ import javax.swing.JOptionPane;
  * @author oquintansocampo
  */
 public class Condicional {
+
     private double numero1;
     private double numero2;
     private double numero3;
-    
-    public Condicional(){
-        
+
+    public Condicional() {
+
     }
-    public double numeros(){
-        String respuesta= JOptionPane.showInputDialog("Introducir número: ");
-        double valor= Double.parseDouble(respuesta);
+
+    public double numeros() {
+        String respuesta = JOptionPane.showInputDialog("Introducir número: ");
+        double valor = Double.parseDouble(respuesta);
         return valor;
     }
-    public void comparar(double n1, double n2, double n3){
-        numero1=n1;
-        numero2=n2;
-        numero3=n3;
-        if (n1>n2&&n1>n3){
-            JOptionPane.showMessageDialog(null, n1);
-            }else if(n2>n1&&n2>n3){
+
+    public void comparar(double n1, double n2, double n3) {
+        numero1 = n1;
+        numero2 = n2;
+        numero3 = n3;
+        if (n1 != n2 && n1 != n3 && n2 != n3) {
+            if (n1 > n2 && n1 > n3) {
+                JOptionPane.showMessageDialog(null, n1);
+            } else if (n2 > n1 && n2 > n3) {
                 JOptionPane.showMessageDialog(null, n2);
-            }else{
+            } else if (n3 > n1 && n3 > n2) {
                 JOptionPane.showMessageDialog(null, n3);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Hay numeros iguales.");
+
         }
     }
 }
